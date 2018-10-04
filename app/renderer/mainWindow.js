0e69155c3a7ad6067924ddb127a886c15e68e68c
Enes Furkan Olcay <fopeak@gmail.com>
2018-10-04 23:32:24 +0300

    Revert "unneeded"
    This reverts commit 88a15f89e8e541a59d3ad58811e7ac76d8b25bb6.

--- app/renderer/mainWindow.js
+++ app/renderer/mainWindow.js
@@ -2,7 +2,8 @@ const electron = require('electron');
 const ipc = electron.ipcRenderer;
 const $ = require('jquery');
 
-$("#router").hide().load('connect.html').fadeIn('slow')
+const router = document.getElementById('router')
+$("#router").hide().load('chat.html').fadeIn('slow')
 
 var exitBtn = document.getElementById('exit')
 var minimizeBtn = document.getElementById('minimize')
